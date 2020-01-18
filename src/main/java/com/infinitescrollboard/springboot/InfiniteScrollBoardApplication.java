e49918f9ab74960a115bf5a1ef46a00779952165
JunKi Kim <zxc7023@gmail.com>
2020-01-18 21:26:01 +0900

    project 생성

---
+++ src/main/java/com/infinitescrollboard/springboot/InfiniteScrollBoardApplication.java
@@ -0,0 +1,13 @@
+package com.infinitescrollboard.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiniteScrollBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfiniteScrollBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infinitescrollboard/springboot/InfiniteScrollBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinitescrollboard.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiniteScrollBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
